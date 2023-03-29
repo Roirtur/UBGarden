e42bf6179020c2410d0787f5f16570bcbf7f9531
Alexandre Laffargue <alexandre.laffargue@etu.u-bordeaux.fr>
2023-03-29 17:03:12 +0200

    Added Carrots and Land classes

---
+++ src/main/java/fr/ubx/poo/ugarden/go/decor/ground/Carrots.java
@@ -0,0 +1,23 @@
+package fr.ubx.poo.ugarden.go.decor.ground;
+
+import fr.ubx.poo.ugarden.game.Position;
+import fr.ubx.poo.ugarden.go.bonus.Bonus;
+import fr.ubx.poo.ugarden.go.personage.Player;
+
+public class Carrots extends Ground{
+    public Carrots(Position position) {
+        super(position);
+    }
+
+    @Override
+    public void takenBy(Player player) {
+        Bonus bonus = getBonus();
+        if (bonus != null) {
+            bonus.takenBy(player);
+        }
+    }
+
+    @Override
+    public int energyConsumptionWalk() { return 1; }
+
+}
