c8e64a82b3cd35a513358990b9a52ac515447202
Arthur MACDONALD <arthur.macdonald@etu.u-bordeaux.fr>
2023-03-29 16:56:19 +0200

    Added new base classes

---
+++ src/main/java/fr/ubx/poo/ugarden/go/decor/Princess.java
@@ -0,0 +1,9 @@
+package fr.ubx.poo.ugarden.go.decor;
+
+import fr.ubx.poo.ugarden.game.Position;
+
+public class Princess extends Decor {
+    public Princess(Position position) {
+        super(position);
+    }
+}
